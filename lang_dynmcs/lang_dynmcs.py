cb673c128ccadb9d068409740e01f24cecb8e216
spectrumtouch <spectrum_touch@yahoo.com>
2016-09-28 23:42:47 -0400

    Additional Edits to lang_dynmcs functions

--- lang_dynmcs/lang_dynmcs.py
+++ lang_dynmcs/lang_dynmcs.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 import math
 from langinput.py import *
-def lang_dynmcs(xi, vi, temp, Kd, dtime, Ttot, Uenergy, Fnet)
+def lang_dynmcs(xi, vi, temp, Kd, dtime, Ttot, Uenergy, Fnet):
 #Calculate position
-    def position(xi,dtime)
+    def position(xi,dtime):
         xnext=xi + dtime*xi
 #Calculate new velocity
-    def velocity(vi,dtime)
+    def velocity(vi,dtime):
         vnext=vi+ dtime*vi
 #Calculate acceleration
-    def forcenet(m,vnext,vi,dtime)
-    Fnet=m(vnext-vi/dtime)
+    def forcenet(m,vnext,vi,dtime):
+    Fnet=m*(vnext-vi/dtime)
 
 #Defining For Loop
 #for T in range(0,Ttot)
