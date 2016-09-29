73bb002313aa6ba532d75b92fb6b3d6b7af13d8a
spectrumtouch <spectrum_touch@yahoo.com>
2016-09-29 00:08:04 -0400

    Fixed Crashing

--- lang_dynmcs/lang_dynmcs.py
+++ lang_dynmcs/lang_dynmcs.py
@@ -4,13 +4,13 @@ from langinput.py import *
 def lang_dynmcs(xi, vi, temp, Kd, dtime, Ttot, Uenergy, Fnet):
 #Calculate position
     def position(xi,dtime):
-        xnext=xi + dtime*xi
+        return xnext=xi + dtime*xi
 #Calculate new velocity
     def velocity(vi,dtime):
-        vnext=vi+ dtime*vi
+        return vnext=vi+ dtime*vi
 #Calculate acceleration
     def forcenet(m,vnext,vi,dtime):
-    Fnet=m*(vnext-vi/dtime)
+        return Fnet=m*(vnext-vi/dtime)
 
 #Defining For Loop
 #for T in range(0,Ttot)
