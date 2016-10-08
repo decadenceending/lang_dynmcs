019e954a17cc332bdc1fbdc2d1dca2a740d317d3
Orkhan Abdullayev <spectrum_touch@yahoo.com>
2016-10-07 23:46:41 -0400

    Code

--- lang_dynmcs/lang_dynmcs.py
+++ lang_dynmcs/lang_dynmcs.py
@@ -3,10 +3,20 @@ import math
 import random
 def lang_dynmcs(xi, vi, temp, coeff, dtime,ttot):
     crs = open(langinput.py, "r")
-    crs = open(Uenergy.txt, "r")
+    #Attempt to take potential energy file and split its columns and create
+    #an array
+    UIndex=[]; Upos=[]; Uenergy=[]; UForce=[];
+    i = open(Uenergy.txt,'r')
+    for line in i:
+    w, x, y, z = line.split(' ',' ',' ',' ',' ')
+    UIndex.append(float(w))
+    Upos.append(float(x))
+    Uenergy.append(float(y))
+    UForce.append(float(z))
+    #Perform a loop for the computations
     for dtime in [(float(j) / 100) for j in range(0, ttot, 1)]:
         random.random()
-        Fnet = ((-vi*coeff)+random*sqrt(2*coeff*temp)-Uenergy)
+        Fnet = ((-vi*coeff)+random*sqrt(2*coeff*temp)-Uenergy[j])
         accl = Fnet/m
         vi = vi + dtime*accl
         xi = xi + dtime*vi
