a7d469183c24b9136a314b930c4e459b502c296f
spectrumtouch <spectrum_touch@yahoo.com>
2016-12-28 21:45:14 -0500

    -Input file read edit

--- lang_dynmcs/langinput.py
+++ lang_dynmcs/langinput.py
@@ -1,9 +1,8 @@
-#Define input variables for Langevin Dynamics Calculations
-#Specify ttot as desired time * 100
-ttot=200
-xi=0
-vi=1
-temp=1
-coeff=1
-dtime=0.01
-m=0.0001
+200
+0
+1
+1
+1
+0.01
+0.0001
+0
