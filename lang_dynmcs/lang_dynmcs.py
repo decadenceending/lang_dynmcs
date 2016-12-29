65fd34c7e5d3c4dcdc02dbd31825f5a39b7789e1
spectrumtouch <spectrum_touch@yahoo.com>
2016-12-28 20:45:40 -0500

    -Coveralls

--- lang_dynmcs/lang_dynmcs.py
+++ lang_dynmcs/lang_dynmcs.py
@@ -9,13 +9,12 @@ vi=1
 temp=1
 coeff=1
 dtime=0.01
-Uenergy1=0.5
 m=0.0001
 jk=0
 
 def force_net(vi,coeff,temp,Uenergy,jk):
-    # Normally use potential energy in the following format Uenergy[j]
 
+    # Normally use potential energy in the following format Uenergy[j]
     Fnet = ((-vi*coeff)+random.random()*math.sqrt(2*coeff*temp)-Uenergy[jk])
     return Fnet
 
@@ -38,7 +37,7 @@ def position(xi,dtime,vi):
     return xi
 
 def potential_energy():
-    with open('Parameter_List.txt') as f:
+    with open("Parameter_List.txt") as f:
         f=[x.strip() for x in f if x.strip()]
         data=[tuple(map(float,x.split())) for x in f[0:]]
         Uenergy=[x[2] for x in data]
@@ -48,12 +47,9 @@ def potential_energy():
 def write_output(index_out,time_out,position_out,velocity_out):
 
     f = open("Output.txt", "w")
-
-
     #Note {0}, {1}, etc, only neede for py26
     for i in range(len(index_out)):
         f.write("{0} {1} {2} {3}\n".format(index_out[i], time_out[i],position_out[i],velocity_out[i]))
-
     f.close()
 
 def lang_dynmcs(xi, vi, temp, m, coeff, dtime,ttot,jk):
